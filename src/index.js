512122f274b4859c70902a80f8eed8c511924c1d
kylewstewart <kyle.w.stewart@mac.com>
2017-11-30 14:52:42 -0500

    Redux Provide React-Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReduxThunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './App';
+import reducers from './reducers';
+
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(ReduxThunk),
+);
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
