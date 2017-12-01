4a39a7d2dd41363841a200074885e604ae094381
kylewstewart <kyle.w.stewart@mac.com>
2017-12-01 18:30:55 -0500

    Reformating

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const HomeContainer = () => <Header contents="Welcome to Your App" />;
+
+export default HomeContainer;
--- src/index.js
+++ src/index.js
@@ -3,20 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './App';
 import store from './store';
-
-// import ReduxThunk from 'redux-thunk';
-// import { applyMiddleware, createStore } from 'redux';
-// import reducers from './reducers';
-// const store = createStore(
-//   reducers,
-//   {},
-//   applyMiddleware(ReduxThunk),
-// );
 
 ReactDOM.render(
   <Provider store={store}>
