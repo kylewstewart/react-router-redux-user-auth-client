07804b5db6c5b3505a921635c11cb3af381e3e7e
kylewstewart <kyle.w.stewart@mac.com>
2017-12-02 00:05:09 -0500

    auth in progress

--- src/containers/LogInContainer.js
+++ src/containers/LogInContainer.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { Form } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import Home from './HomeContainer';
+import { logIn } from '../actions';
 
-
-import * as adapters from '../adapters';
+const propTypes = {
+  logIn: PropTypes.func.isRequired,
+  isAuth: PropTypes.bool.isRequired,
+};
 
 class LogInContainer extends Component {
   state = {
@@ -13,35 +19,43 @@ class LogInContainer extends Component {
 
   handleChange= (e, { name, value }) => this.setState({ [name]: value });
 
-  handleClick = () => {
-    adapters.auth(this.state.username, this.state.password)
-      .then(response => console.log(response));
-  };
+  handleClick = () => this.props.logIn(this.state.username, this.state.password);
 
   render() {
-    return (
-      <Form>
-        <Form.Input
-          label="Username"
-          name="username"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <Form.Input
-          label="Password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <Form.Button
-          content="Submit"
-          onClick={this.handleClick}
-        />
-      </Form>
-    );
+    if (!this.props.isAuth) {
+      return (
+        <Form>
+          <Form.Input
+            label="Username"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label="Password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <Form.Button
+            content="Submit"
+            onClick={this.handleClick}
+          />
+        </Form>
+      );
+    }
+
+    return <Redirect to={Home} />;
   }
 }
 
-const mapStateToProps = state => ({ state });
+LogInContainer.propTypes = propTypes;
 
-export default connect(mapStateToProps)(LogInContainer);
+const mapStateToProps = ({ auth }) => (
+  {
+    isAuth: auth.isAuth,
+    isFetching: auth.isFetching,
+  }
+);
+
+export default connect(mapStateToProps, { logIn })(LogInContainer);
